2fa6ec5204ae5e29daf0278019bdd7dfe436f9ed
t0ma999 <idkme@bk.ru>
2026-03-11 11:49:35 +0300

    SleepSort

--- src/SleepSort.java
+++ src/SleepSort.java
@@ -1,9 +1,15 @@
+/*
+    Для каждого числа запускается поток
+    Поток спит столько миллисекунд, сколько равно числу
+    Когда поток просыпается - он печатает число
+ */
+
 public class SleepSort {
     public static void sleepSort(int[] arr) {
         for (int num : arr) {
             new Thread(() -> {
                 try {
-                    Thread.sleep(num * 100);
+                    Thread.sleep(num * 100L);
                     System.out.print(num + " ");
                 } catch (InterruptedException e) {
                     e.printStackTrace();
